5019181ee626edcc3f85a1bdec3a76872fce6050
aspdiscovery123 <48548207+aspdiscovery123@users.noreply.github.com>
2023-09-21 18:03:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 17:59:18 2023
+
+@author: DELL
+"""
+
+from flask import Flask,request
+
+app=Flask(__name__)
+
+@app.route('/home')
+
+def application():
+    return "hello from ashish"
+
+@app.route('/setup',methods=['POST'])
+
+def prediction():
+    data=request.get_json(force=True)
+    print(data)
+    return "done"
+
+app.run(host='0.0.0.0')
